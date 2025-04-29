36cfbaaf45b6f455072231d080fe149315fbf3e3
James Bosley <jamesbosley123@gmail.com>
2025-04-29 16:46:19 -0500

    Added conditionals, try catch blocks, loop screens and filewriters to the app

--- src/main/java/com/pluralsight/BankAccountLedgerApp.java
+++ src/main/java/com/pluralsight/BankAccountLedgerApp.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class BankAccountLedgerApp {
@@ -18,48 +20,109 @@ public class BankAccountLedgerApp {
         }
     }
     static void appHome() {
-        //boolean that will run loop
-        boolean appRunning = true;
+        try {
+            //boolean that will run loop
+            boolean appRunning = true;
 
-        //loops through home screen options
-        while(appRunning) {
-            System.out.println("Welcome to the Ace Account Home Screen! Select a letter from our list of options \n" +
-                    "D) Add Deposit \n" +
-                    "P) Make Payment (Debit) \n" +
-                    "L) Ledger \n" +
-                    "X) Exit");
-            String userChoice = scan.nextLine();
-            //checks which options chosen and calls a method or ends loop based on it
-            if(userChoice.equalsIgnoreCase("d")) {
+            //file writer to write to file
+            FileWriter writer = new FileWriter("src/main/resources/transacpract.csv");
+            BufferedWriter bufWriter = new BufferedWriter(writer);
 
-            } else if(userChoice.equalsIgnoreCase("p")) {
+            //writes the header for the file
+            bufWriter.write("date|time|description|vendor|amount");
 
-            } else if(userChoice.equalsIgnoreCase("l")) {
-
-            } else if(userChoice.equalsIgnoreCase("x")) {
-                appRunning = false;
+            //loops through home screen options
+            while(appRunning) {
+                System.out.println("Welcome to the Ace Account Home Screen! Select a letter from our list of options \n" +
+                        "D) Add Deposit \n" +
+                        "P) Make Payment (Debit) \n" +
+                        "L) Ledger \n" +
+                        "X) Exit");
+                String userChoice = scan.nextLine();
+                //checks which options chosen and calls a method or ends loop based on it
+                if(userChoice.equalsIgnoreCase("d")) {
+                    addDeposit();
+                    continue;
+                } else if(userChoice.equalsIgnoreCase("p")) {
+                    makePayment();
+                    continue;
+                } else if(userChoice.equalsIgnoreCase("l")) {
+                    ledgerView();
+                    continue;
+                } else if(userChoice.equalsIgnoreCase("x")) {
+                    appRunning = false;
+                }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-    static addDeposit(boolean value) {
-        if(value) {
+    static void addDeposit() {
+        try {
 
-        } else {
-
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-    static makePayment(boolean value) {
-        if(value) {
+    static void makePayment() {
+        try {
 
-        } else {
-
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-    static LedgerView(boolean value) {
-        if(value) {
+    static void ledgerView() {
+        try {
+            boolean onLedger = true;
+            //loops through ledger options
+            while(onLedger) {
+                System.out.println("Welcome to the Ace Accounting Ledger where you can view accounts entries based on criteria. \n" +
+                        "A) All \n" +
+                        "D) Deposits \n" +
+                        "P) Payments \n" +
+                        "R) Reports \n" +
+                        "H) Home");
+                String ledgerChoice = scan.nextLine();
+                if(ledgerChoice.equalsIgnoreCase("a")) {
 
-        } else {
+                } else if(ledgerChoice.equalsIgnoreCase("d")) {
 
+                } else if(ledgerChoice.equalsIgnoreCase("p")) {
+
+                } else if(ledgerChoice.equalsIgnoreCase("r")) {
+                    //loops through report options
+                    boolean onReports = true;
+                    while(onReports) {
+                        System.out.println("Report page. Click one of the numbers to select one of our options. \n" +
+                                "1) Month To Date \n" +
+                                "2) Previous Month \n" +
+                                "3) Year To Date \n" +
+                                "4) Previous Year \n" +
+                                "5) Search by Vendor \n" +
+                                "0) Back");
+                        String reportsChoice = scan.nextLine();
+                        if(reportsChoice.equals("1")) {
+
+                        } else if(reportsChoice.equals("2")) {
+
+                        } else if(reportsChoice.equals("3")) {
+
+                        } else if(reportsChoice.equals("4")) {
+
+                        } else if(reportsChoice.equals("5")) {
+
+                        } else if(reportsChoice.equals("0")) {
+                            break;
+                        }
+                    }
+
+                } else if(ledgerChoice.equalsIgnoreCase("h")) {
+                    onLedger = false;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
